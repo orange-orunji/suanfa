19cf3d092f592daff5626c4f5a8349f3ef4de9e7
orange-orunji <3078045959@qq.com>
2026-09-07 11:32:53 +0800

    refactor(Hash): 优化字母异位词分组算法实现
    - 重构Solution类中的groupAnagrams方法逻辑
    - 使用HashMap存储排序后的字符串作为键值
    - 通过字符数组排序实现字母异位词识别
    - 优化列表构建过程，提高代码可读性
    - 移除原有实现并采用更清晰的分组策略

--- hot100/Hash/src/LeetCode_49.java
+++ hot100/Hash/src/LeetCode_49.java
@@ -13,3 +13,23 @@ public class LeetCode_49 {
         return new ArrayList<>(map.values());
     }
 }
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map = new HashMap<>();
+        int n = strs.length;
+        for(int i = 0 ;i < n; i++){
+            String str = strs[i];
+            char[] ls = str.toCharArray();
+            Arrays.sort(ls);
+            String nstr = new String(ls);
+            List<String> list = map.get(nstr);
+            if(list == null){
+                list = new ArrayList<>();
+                map.put(nstr,list);
+            }
+            list.add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
