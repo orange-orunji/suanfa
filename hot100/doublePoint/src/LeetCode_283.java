589023e39d63e91b126dac842f80e47d6208c65f
orange-orunji <3078045959@qq.com>
2026-09-07 13:52:15 +0800

    refactor(doublePoint): 优化移动零元素算法实现
    - 使用双指针技术重写数组遍历逻辑
    - 简化了元素交换过程，提高代码可读性
    - 减少了不必要的变量声明，优化内存使用
    - 保持了原算法的时间复杂度 O(n) 性能
    - 通过单次遍历完成零元素移动操作

--- hot100/doublePoint/src/LeetCode_283.java
+++ hot100/doublePoint/src/LeetCode_283.java
@@ -17,3 +17,18 @@ public void moveZeroes(int[] nums) {
         nums[y] = temp;
     }
 }
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0,right = 0,n = nums.length;
+        while(right < n){
+            if(nums[right]!=0){
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
+            }
+            right++;
+        }
+    }
+}
